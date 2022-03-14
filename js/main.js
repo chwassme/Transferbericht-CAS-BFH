470f3592bd2b67ee7daf0fc2ef4868fa71338ccf
Christian Wassmer <christian.wassmer@gmail.com>
2022-03-14 13:05:34 +0100

    write heartrate and sleep

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import dayjs from "dayjs";
 import storage from "node-persist";
+import fs from "fs";
 import {GarminConnect} from 'garmin-connect';
 
 main();
-
 
 async function main() {
     await storage.init();
@@ -18,14 +18,56 @@ async function main() {
         await storage.setItem('session', session)
     });
 
-    const startDate = new Date(2022, 2, 10);
-    const endDate = new Date(2022, 2, 14);
+    const startDate = new Date(2021, 4, 20);
+    const endDate = new Date(2021, 4, 20);
 
     let currentDate = dayjs(startDate);
+    const sleepData = [];
+    let sleepKeys;
+
+    let heartRateKeys = [
+        'calendarDate'
+        , 'maxHeartRate'
+        , 'minHeartRate'
+        , 'restingHeartRate'
+        , 'lastSevenDaysAvgRestingHeartRate'
+    ];
+    const heartRateData = [heartRateKeys.join(';')];
 
     while (currentDate.isBefore(endDate) || currentDate.isSame(endDate)) {
+        const curDate = currentDate.toDate();
+
+        const sleep = await GCClient.getSleep(curDate);
+        if (!sleepKeys) {
+            sleepKeys = Object.keys(sleep).join(';');
+            sleepData.push(sleepKeys);
+        }
+        sleepData.push(Object.keys(sleep).map(key => sleep[key]).join(';'));
+
+        const heartRate = await GCClient.getHeartRate(curDate);
+        heartRateData.push(heartRateKeys.map(key => heartRate[key]).join(';'));
+
+        // {
+        //   "startGMT": "2022-03-10T07:45:00.0",
+        //   "endGMT": "2022-03-10T08:00:00.0",
+        //   "steps": 0,
+        //   "primaryActivityLevel": "sedentary", // sedentary, sleeping, active, generic, none, highlyActive, ...
+        //   "activityLevelConstant": true
+        // }
+        const steps = await GCClient.getSteps(curDate);
+
+
+        console.log(curDate, sleep.id, sleep.deepSleepSeconds, steps.length, heartRate.restingHeartRate)
+
         currentDate = currentDate.add(1, "days");
-        const sleep = await GCClient.getSleep(currentDate.toDate());
-        console.log(sleep.id, sleep.deepSleepSeconds)
     }
+
+
+    fs.writeFileSync('data/sleep.csv', sleepData.join('\n'), {flag: 'w'}, err => {
+        console.error(err)
+    })
+
+    fs.writeFileSync('data/heartrate.csv', heartRateData.join('\n'), {flag: 'w'}, err => {
+        console.error(err)
+    })
 }
