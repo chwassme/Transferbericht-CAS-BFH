c627e3a3495b186ee354a34e66312592053a8be6
Christian Wassmer <christian.wassmer@gmail.com>
2022-03-14 10:47:00 +0100

    garmin data project

--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@ async function main() {
     const storage = require('node-persist');
     await storage.init();
     const session = await storage.getItem('session');
-    console.log("restore session: " + JSON.stringify(session));
-
 
     // Create a new Garmin Connect Client
     const { GarminConnect } = require('garmin-connect');
@@ -14,8 +12,10 @@ async function main() {
     GCClient.restore(session);
 
     GCClient.onSessionChange(async session => {
-        console.log("on session change")
         await storage.setItem('session', session)
     });
+
+    const data = await GCClient.getSleep();
+    console.log(data)
 
 }
